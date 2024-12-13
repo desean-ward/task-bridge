b8c183a093fcfd906f17e42108127ddbd695d303
De Sean Ward <d.ward.chi@gmail.com>
2024-12-13 03:45:42 -0500

    Testimonials section animations

--- src/components/testimonials/testimonials.component.tsx
+++ src/components/testimonials/testimonials.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 import {
   TestimonialCards,
   TestimonialsContainer,
@@ -6,19 +6,50 @@ import {
 } from "./testimonials.styles";
 import testimonials from "@/data/testimonials.json";
 import Testimonial from "../testimonial-card/testimonial-card.component";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Testimonials = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(() => {
+    gsap.from(".heading", {
+      opacity: 0,
+      y: 100,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: "#testimonials",
+        start: "top 70%",
+        toggleActions: "play reverse restart reverse",
+      },
+    });
+
+    gsap.from("#testimonial-cards", {
+      opacity: 0,
+      y: 100,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: "#testimonial-cards",
+        start: "top 70%",
+        toggleActions: "play reverse play reverse",
+      },
+    });
+  });
+
   return (
     <TestimonialsWrapper id='testimonials'>
       <TestimonialsContainer>
-        <h2 className='text-5xl font-bold mb-8'>
+        <h2 className='heading text-5xl font-bold mb-8'>
           Customer <span className='text-accent'>Testimonials</span>
         </h2>
 
-        <p>Task Bridge transformed our Instagram engagement strategy.</p>
+        <p className='heading'>
+          Task Bridge transformed our Instagram engagement strategy.
+        </p>
 
         {/* Customer Testimonials */}
-        <TestimonialCards>
+        <TestimonialCards id='testimonial-cards'>
           {testimonials.map((person, idx) => (
             <Testimonial key={idx} person={person} />
           ))}
